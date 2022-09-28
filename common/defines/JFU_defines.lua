982789be67983ad174e4c1aa892d0d72ab8cb2a1
johansen <jogiosten@mein.gmx>
2022-09-28 16:15:55 +0200

    fist BBA lone update(still missing a lot as well as a lot of bugs)

--- common/defines/JFU_defines.lua
+++ common/defines/JFU_defines.lua
@@ -8,7 +8,6 @@ NDefines.NMilitary.DEPLOY_TRAINING_MAX_LEVEL = 2;
 NDefines.NMilitary.PROMOTE_LEADER_CP_COST = 1.0;	
 NDefines.NMilitary.UNIT_LEADER_ASSIGN_TRAIT_COST = 0.00;
 NDefines.NMilitary.UNIT_LEADER_MODIFIER_COOLDOWN_ON_GROUP_CHANGE = 0;
-NDefines.NMilitary.LAND_AIR_COMBAT_MAX_PLANES_PER_ENEMY_WIDTH = 1.5;
 
 NDefines.NCountry.POPULATION_YEARLY_GROWTH_BASE = 0.0;
 NDefines.NCountry.BASE_FUEL_CAPACITY = 100000;
@@ -44,16 +43,6 @@ NDefines.NAir.AIR_DEPLOYMENT_DAYS = 0;
 NDefines.NAir.EFFICIENCY_REGION_CHANGE_DAILY_GAIN_STRATEGIC_BOMBER = 0.036;
 NDefines.NAir.EFFICIENCY_REGION_CHANGE_DAILY_GAIN_TACTICAL_BOMBER = 0.096;		
 NDefines.NAir.SUPPLY_NEED_FACTOR = 0.14;
-
-NDefines.NAir.AIR_WING_MAX_SIZE = 800;                               -- this can be halved 3 times into 100 stacks (very convenient)
-NDefines.NAir.DETECT_CHANCE_FROM_AIRCRAFTS_EFFECTIVE_COUNT = 1500;  -- WAS 3000, halved because plane counts halved | Max amount of aircrafts in region to give full detection bonus.
-NDefines.NAir.AIR_WING_COUNTRY_XP_FROM_TRAINING_FACTOR = 0.02; 		-- WAS 0.005, doubled|quadrupeln  to compensate for halved starting planes | Factor on country Air XP gained from wing training
-NDefines.NAir.FIELD_EXPERIENCE_SCALE = 0.004;
-NDefines.NAir.CLOSE_AIR_SUPPORT_EXPERIENCE_SCALE = 0.002;
-NDefines.NAir.BOMBING_DAMAGE_EXPERIENCE_SCALE = 0.0004;						-- WAS 0.001 | This affects how much XP you get from various missions (AFAIK this will affect XP gained from bombing and CAS missions as well)
-NDefines.NAir.COMBAT_MAX_WINGS_AT_ONCE = 5000;                       -- WAS 10000 | Max amount of air wings in one combat simulation. The higher value, the quicker countries may loose their wings. It's a gameplay balance value.
-NDefines.NAir.COMBAT_MAX_WINGS_AT_GROUND_ATTACK = 5000;               -- WAS 10000 | we can really pounce a land strike and escalate
-NDefines.NAir.COMBAT_MAX_WINGS_AT_ONCE_PORT_STRIKE = 5000;       -- WAS 10000 | we can really pounce a naval strike and escalate
 NDefines.NAir.MISSION_COMMAND_POWER_COSTS = {  -- command power cost per plane to create a mission
 	0.0, -- AIR_SUPERIORITY
 	0.0, -- CAS
@@ -71,9 +60,6 @@ NDefines.NAir.MISSION_COMMAND_POWER_COSTS = {  -- command power cost per plane t
 	0.0, -- NAVAL_MINES_SWEEPING
 	0.0, -- MISSION_RECON
 	};
-NDefines.NAir.NAVAL_COMBAT_EXTERNAL_PLANES_JOIN_RATIO = 0.03;
-NDefines.NNavy.NAVAL_COMBAT_AIR_SUB_DETECTION_EXTERNAL_FACTOR = 2.0;
-NDefines.NNavy.NAVAL_COMBAT_AIR_PLANE_COUNT_TO_SUB_DETECTION = 2.0;
 NDefines.NNavy.MISSION_SUPREMACY_RATIOS = { 						-- supremacy multipliers for different mission types   | Strikeforce/Naval Invasion Support now 0 supremacy to avoid free supremacy
 		0.0, -- HOLD
 		1.0, -- PATROL		
@@ -86,11 +72,9 @@ NDefines.NNavy.MISSION_SUPREMACY_RATIOS = { 						-- supremacy multipliers for d
 		0.0, -- RESERVE_FLEET
 		0.2, -- NAVAL_INVASION_SUPPORT
 	};
-NDefines.NAir.COMBAT_DAMAGE_SCALE = 0.05;
 
 NDefines.NNavy.TRAINING_ACCIDENT_CHANCES = 0;
 NDefines.NNavy.NAVAL_MINES_IN_REGION_MAX = 1.0;
-NDefines.NNavy.SUPREMACY_PER_SHIP_BASE =  15                        -- WAS 100 | reduced to relatively nerf subs in terms of naval supremacy, as most of their supremacy comes from a base value of 100; At HFU values
 NDefines.NNavy.NAVAL_TRANSFER_BASE_SPEED = 10                       -- WAS 6 | Increased so moving troops around the world is faster and theaters dont seem so disconnected (particularly aimed at improving Allied gameplay experience)
 NDefines.NNavy.ADMIRAL_TASKFORCE_CAP = 20                           -- WAS 10 | Increased so players can use their same submarine or escort admiral without penalties
 NDefines.NNavy.NAVAL_MINES_DECAY_AT_PEACE_TIME = 1                    -- WAS 0.25 
