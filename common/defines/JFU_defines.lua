449a821fd92cbe1f2de2be298e1f539207c3cbca
johansen <jogiosten@mein.gmx>
2023-04-11 17:28:49 +0200

    fixes

--- common/defines/JFU_defines.lua
+++ common/defines/JFU_defines.lua
@@ -26,6 +26,7 @@ NDefines.NCountry.SCORCHED_EARTH_STATE_COST = 999
 NDefines.NCountry.REINFORCEMENT_MANPOWER_DELIVERY_SPEED = 20.0
 NDefines.NCountry.REINFORCEMENT_MANPOWER_CHUNK = 0.2				-- Chunk size of manpower reinforcement delivery, in % of total manpower needed by the template.
 NDefines.NCountry.WAR_SUPPORT_DEFENSIVE_WAR = 0.3				--vanilla 0.2 Impact of being in defensive war
+NDefines.NCountry.AIR_SUPPLY_CONVERSION_SCALE = 0.01
 
 NDefines.NPolitics.LEADER_TRAITS_XP_SHOW = 0.01
 
