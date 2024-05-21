28bc88b2b26d0678afe1f834b01075de23b19e58
Johannsen <jogiosten@mein.gmx>
2024-05-21 14:25:01 +0200

    more dynamic modifiers

--- common/defines/JFU_defines.lua
+++ common/defines/JFU_defines.lua
@@ -80,7 +80,8 @@ NDefines.NDiplomacy.MAX_OPINION_FROM_VOLUNTEERS = 50
 NDefines.NDiplomacy.DIPLOMACY_HOURS_BETWEEN_REQUESTS = 2
 NDefines.NDiplomacy.BASE_SEND_ATTACHE_COST = 0				-- vanilla 100 Political power cost to send attache
 NDefines.NDiplomacy.BASE_SEND_ATTACHE_CP_COST = 0			-- vanilla 50 Command Power sent attache usage cost
-
+NDefines.NDiplomacy.EMBARGO_COST = 999							-- vanilla 100	-- One-time cost
+NDefines.NDiplomacy.REVOKE_EMBARGO_COST = 999						-- vanilla 0	-- Cost to remove an existing embargo
 
 
 NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 480			
